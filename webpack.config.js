ccac3c5ce9e810742e2bc585bc84e2772e93186c
zengzijian <zengzj@shinetech-china.com>
2018-11-19 17:22:27 +0800

    打包后，js和css分离，打包解决缓存问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
@@ -9,9 +10,10 @@ module.exports = {
         filename: "bundle.js"//打包后输出文件的文件名
     },
     devServer: {
-        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        contentBase: "./build",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
-        inline: true//实时刷新
+        inline: true,//实时刷新
+        hot: true
     },
     module: {
         rules: [
@@ -46,6 +48,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
         }),
-        new webpack.HotModuleReplacementPlugin()//热加载插件
+        new webpack.HotModuleReplacementPlugin(),//热加载插件
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        // new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css")
     ],
 }
