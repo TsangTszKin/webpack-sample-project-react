7a998502bee58bf98a28a13b9475cb1400fad579
zengzijian <zengzj@shinetech-china.com>
2018-11-20 08:40:54 +0800

    过滤node_modules

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,12 +5,18 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 
 module.exports = {
-    entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
+    entry: __dirname + "/src/main.js", //已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/build", //打包后的文件存放的地方
+        path: __dirname + "/dist", //打包后的文件存放的地方
         filename: "bundle-[hash].js" //打包后输出文件的文件名
     },
     devtool: "null",
+    resolve: {
+        alias: {
+            '@': path.resolve("src")
+        },
+        extensions: ['*', '.js', '.jsx', '.json', '.less', '.css']
+    },
     module: {
         rules: [
             {
@@ -38,7 +44,7 @@ module.exports = {
     plugins: [
         new webpack.BannerPlugin('版权所有，翻版必究'),
         new HtmlWebpackPlugin({
-            template: __dirname + "/app/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
+            template: __dirname + "/src/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
         }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.optimize.OccurrenceOrderPlugin(),
